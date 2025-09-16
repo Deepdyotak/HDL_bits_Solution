3af3e020222629cdf68d304defbf44e610ff3992
Deep Dyotak Dash <deepdyotakdash1234@gmail.com>
2025-09-16 11:27:23 +0530

    Update dual edge triggered ff.v

--- Sequential_Logic/Latches_&_Flip-flops/dual edge triggered ff.v
+++ Sequential_Logic/Latches_&_Flip-flops/dual edge triggered ff.v
@@ -9,6 +9,7 @@ module top_module (
     always@(posedge clk) q1<=d;
     always@(negedge clk) q2<=d;
     assign q = clk ? q1:q2;  //because q1 represents value of d at last rising edge 
+// clk = 1 means it just got value of last rising edge and similarly clk = 0 means it just got value of falling edge
 
 
     
